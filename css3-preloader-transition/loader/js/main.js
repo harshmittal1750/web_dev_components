4ef3c27bf62b9e87a27c098df29f444d30a2adb4
harshmittal1750 <harshmittal1750@gmail.com>
2021-01-31 19:29:37 +0530

    preloader

---
+++ css3-preloader-transition/loader/js/main.js
@@ -0,0 +1,13 @@
+/* 
+
+Oh nice, welcome to the js file of dreams.
+Enjoy responsibly!
+@harshmittal1750
+*/
+
+$(document).ready(function () {
+  setTimeout(function () {
+    $("body").addClass("loaded");
+    $("h1").css("color", "#222222");
+  }, 3000);
+});
